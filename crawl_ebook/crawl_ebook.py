a88427570db3e8d6efe7ed91a61aa8b3c5303275
skyge <18844062006@163.com>
2017-09-30 19:48:25 +0800

    disguising as browser

--- crawl_ebook/crawl_ebook.py
+++ crawl_ebook/crawl_ebook.py
@@ -1,3 +1,4 @@
+# -*- coding:UTF-8 -*-
 from urllib import request
 from bs4 import BeautifulSoup
 
@@ -7,13 +8,17 @@ import random
 article_url = 'http://www.kanunu8.com/book4/10204/'
 content_url = int(226613)
 while content_url < 226730:
-    html = request.urlopen(article_url + str(content_url) + '.html')
-    bsObj = BeautifulSoup(html, 'html.parser')
+    head = {}
+    head['User-Agent'] = 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19'
+    url = article_url + str(content_url) + '.html'
+    download_req = request.Request(url=url, headers=head)
+    download_response = request.urlopen(download_req)
+    bsObj = BeautifulSoup(download_response, 'html.parser')
     title = bsObj.font.string
     comment = bsObj.p.get_text()
     with open('ebook.txt', 'a', encoding='utf-8') as file:
         file.write(title)
         file.write(comment)
     content_url += 1
-    num = random.randint(5, 10)
+    num = random.randint(8, 13)
     time.sleep(num)
